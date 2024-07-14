2aa0d8af5d1cf07119fab3dd2941e539f8733cf5
Michael Sawitzki <m.sawitzki99@gmail.com>
2024-07-14 20:19:01 +0200

    Fix test

--- tests/test_fifa.py
+++ tests/test_fifa.py
@@ -7,7 +7,6 @@ import os
 from fifa21.make_dataset import main
 from tests import _PATH_DATA_FIFA
 
-data = pd.read_csv(os.path.join(_PATH_DATA_FIFA, "raw/fifa21_raw_data_v2.csv"))
 
 @pytest.mark.skipif(not os.path.exists(_PATH_DATA_FIFA), reason="Data files not found")
 def test_fifa_data_loading():
@@ -17,6 +16,7 @@ def test_fifa_data_loading():
 
 @pytest.mark.skipif(not os.path.exists(_PATH_DATA_FIFA), reason="Data files not found")
 def test_make_dataset():
+    data = pd.read_csv(os.path.join(_PATH_DATA_FIFA, "raw/fifa21_raw_data_v2.csv"))
     main(data)
 
 
